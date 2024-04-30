82bd7ad74a194eeb7d6ab6222cf3a07e164912b5
SamTheDev <samthedev32@gmail.com>
2024-04-30 17:22:42 +0200

    dunno

--- blink/main.c
+++ blink/main.c
@@ -1,15 +1,23 @@
 #include "boards/pico.h"
 #include "pico/stdlib.h"
 
+#define POWEROFF_PIN 15
+
 int main() {
   gpio_init(PICO_DEFAULT_LED_PIN);
-
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-  while (true) {
+  gpio_init(POWEROFF_PIN);
+  gpio_set_dir(POWEROFF_PIN, GPIO_OUT);
+
+  gpio_put(POWEROFF_PIN, false);
+
+  for (int i = 0; i < 5; i++) {
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    sleep_ms(500);
+    sleep_ms(100);
     gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    sleep_ms(500);
+    sleep_ms(100);
   }
+
+  gpio_put(POWEROFF_PIN, true);
 }
